38c06fc576975ffd0bd6427f86776d69fecabf3e
Andrey Tararaksin <a.tararaksin@skoltech.ru>
2019-10-09 22:22:40 +0300

    Download with retries

--- src/main/scala/TileLoader.scala
+++ src/main/scala/TileLoader.scala
@@ -3,6 +3,7 @@ package io.geoalert.tileloader
 
 import java.nio.file.{Files, Path, Paths}
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
@@ -20,6 +21,7 @@ import akka.stream.scaladsl.{FileIO, Sink, Source}
 import cats.implicits._
 import com.typesafe.scalalogging.StrictLogging
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 object TileLoader extends App with StrictLogging {
@@ -63,28 +65,33 @@ object TileLoader extends App with StrictLogging {
     .sorted
     .zipWithIndex
 
-  def downloadTile(key: SpatialKey, n: Int) = {
-    val url = tileUrl.replace("{z}", z.toString)
-      .replace("{x}", key.col.toString)
-      .replace("{y}", key.row.toString)
+  def downloadTile(key: SpatialKey, n: Int, attempts: Int): Future[Done] = {
+    if (attempts == 0) {
+      Future.failed(new RuntimeException(s"Exceeded amount of attemps for $key"))
+    } else {
+      val url = tileUrl.replace("{z}", z.toString)
+        .replace("{x}", key.col.toString)
+        .replace("{y}", key.row.toString)
 
-    def responseOrFail[T](responseTry: (Try[HttpResponse], T)) = responseTry match {
-      case (Success(r), _) if r.status.isSuccess() => r
-      case _ => sys.error(s"Unable to download from $url")
+      def responseOrFail[T](responseTry: (Try[HttpResponse], T)) = responseTry match {
+        case (Success(r), _) if r.status.isSuccess() => r
+        case _ => sys.error(s"Unable to download from $url")
+      }
+
+      def writeFile(dst: Path)(httpResponse: HttpResponse) =
+        httpResponse.entity.dataBytes.runWith(FileIO.toPath(dst))
+
+      val requestResponseFlow = Http().superPool[Unit](settings = conSettings)
+
+      val path = getTilePath(key, n)
+      path.getParent.toFile.mkdirs()
+
+      Source.single((Get(url), ()))
+        .via(requestResponseFlow)
+        .map(responseOrFail)
+        .runWith(Sink.foreach(writeFile(path)))
+        .recoverWith { case _ => downloadTile(key, n, attempts - 1) }
     }
-
-    def writeFile(dst: Path)(httpResponse: HttpResponse) =
-      httpResponse.entity.dataBytes.runWith(FileIO.toPath(dst))
-
-    val requestResponseFlow = Http().superPool[Unit](settings = conSettings)
-
-    val path = getTilePath(key, n)
-    path.getParent.toFile.mkdirs()
-
-    Source.single((Get(url), ()))
-      .via(requestResponseFlow)
-      .map(responseOrFail)
-      .runWith(Sink.foreach(writeFile(path)))
   }
 
   logger.info(s"Starting to download tiles from $tileUrl to $targetPath")
@@ -92,19 +99,21 @@ object TileLoader extends App with StrictLogging {
   system.scheduler.schedule(
     0 seconds,
     300 seconds,
-    () => logger.info(s"Current amount of tiles loaded: ${Files.list(targetPath).count()}")
+    () => {
+      logger.info(s"Current amount of tiles loaded: ${Files.walk(targetPath, 2).count()}")
+    }
   )
 
   def batchDownload(keys: List[(SpatialKey, Int)], batchSize: Int): Unit = {
     if (!keys.isEmpty) {
       val (batch, tail) = keys.splitAt(batchSize)
       batch.filterNot(e => Files.exists(getTilePath(e._1, e._2)))
-        .traverse(e => downloadTile(e._1, e._2))
+        .traverse(e => downloadTile(e._1, e._2, 5))
         .onComplete {
           case _ => batchDownload(tail, batchSize)
         }
     }
   }
 
-  batchDownload(keys, 100)
+  batchDownload(keys, 256)
 }
